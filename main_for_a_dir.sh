a1c83ce00bafeb6ce9770a16ff98914dfab6a531
arppy <berta@inf.u-szeged.hu>
2024-06-05 13:26:23 +0200

    gpu param to script param

--- main_for_a_dir.sh
+++ main_for_a_dir.sh
@@ -2,4 +2,9 @@
 
 #example: $1 == "/home/hegedusi/backdoor_models/R18_imagenette_robust/"
 suffix=$(basename $1)
-for i in $1*pth ; do python main.py --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
+if [ $# -lt 2 ]; then
+  gpu=0
+else
+  gpu=$2
+fi
+for i in $1*pth ; do python main.py --device $gpu --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
