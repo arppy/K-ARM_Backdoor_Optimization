77959eefeb925120e802095ad0c2f5b42a0d49bf
arppy <berta@inf.u-szeged.hu>
2024-06-05 12:01:49 +0200

    debug script pth

--- main_for_a_dir.sh
+++ main_for_a_dir.sh
@@ -2,4 +2,4 @@
 
 #example: $1 == "/home/hegedusi/backdoor_models/R18_imagenette_robust/"
 suffix=$(basename $1)
-for i in $1 ; do python main.py --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
+for i in $1*pth ; do python main.py --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
