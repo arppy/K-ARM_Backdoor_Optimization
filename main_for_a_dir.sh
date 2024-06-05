a8eb5b517d9e41e85e4b6348510429339c0d5e9c
arppy <berta@inf.u-szeged.hu>
2024-06-05 17:21:26 +0200

    debug step

--- main_for_a_dir.sh
+++ main_for_a_dir.sh
@@ -18,7 +18,7 @@ else
   steps=$4
 fi
 
-for i in $1*pth ; do python main.py --device $gpu --beta $beta --steps $steps --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
+for i in $1*pth ; do python main.py --device $gpu --beta $beta --step $steps --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
 
 # AUC
 # awk '{if($2~/-/){print(1,$16,$18)}else{print(0,$16,$18)}}' results_R18_imagenette_different_initseed | | ./../torch_dna/auc.sh 1 2
