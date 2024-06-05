b01fe116a07b34e621c5d44aaef23f0a349aadda
arppy <berta@inf.u-szeged.hu>
2024-06-05 11:58:39 +0200

    a script to run more main.py

---
+++ main_for_a_dir.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#example: $1 == "/home/hegedusi/backdoor_models/R18_imagenette_robust/"
+suffix=$(basename $1)
+for i in $1 ; do python main.py --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
