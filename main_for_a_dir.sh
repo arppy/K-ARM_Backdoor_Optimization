16be6b16d16ded88c1aa3d1cc190f5ed318674d3
arppy <berta@inf.u-szeged.hu>
2024-09-20 10:44:41 +0200

    paramstr

--- main_for_a_dir.sh
+++ main_for_a_dir.sh
@@ -17,8 +17,13 @@ if [ $# -lt 4 ]; then
 else
   steps=$4
 fi
+if [ $# -lt 5 ]; then
+  paramstr=""
+else
+  paramstr=$5
+fi
 
-for i in $1*pth ; do python main.py --device $gpu --beta $beta --step $steps --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
+for i in $1*pth ; do python main.py $paramstr --device $gpu --beta $beta --step $steps --num_classes 10 --local_theta 0.0 --result_filepath "results_"$suffix --examples_dirpath examples_dirpath/ --model_filepath $i; done
 
 # AUC
 # awk '{if($2~/-/){print(1,$16,$18)}else{print(0,$16,$18)}}' results_R18_imagenette_different_initseed | | ./../torch_dna/auc.sh 1 2
